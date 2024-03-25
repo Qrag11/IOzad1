eaac76e9a971c366ce7fde1e070e2bdd9797b075
Qrag11 <m.przystawski8@gmail.com>
2024-03-25 17:43:11 +0100

    Dodanie funkcji sprawdzajacej czy liczba jest pierwsza

--- projekt1.cpp
+++ projekt1.cpp
@@ -2,6 +2,24 @@
 
 using namespace std;
 
+void czyPierwsza(int n){
+
+	if(n<2)
+		cout<<"Nie jest pierwsza";
+
+	bool czyPierwsza = true;
+	for(int i=2;i*i<=n;i++){
+		if(n%i==0)
+			czyPierwsza=false;
+	}
+
+	if(czyPierwsza == true)
+		cout<<"Jest to liczba pierwsza("<<n<<")"<<endl;
+	else
+		cout<<"Nie jest to lcizba pierwsza ("<<n<<")"<<endl;
+
+}
+
 int main() {
     int a, b;
     cout << "Podaj liczbe calkowita ";
