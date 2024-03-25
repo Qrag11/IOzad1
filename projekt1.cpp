1cd9739bcc731a651d8144aebea7ec0e8c805e4c
Qrag11 <m.przystawski8@gmail.com>
2024-03-25 17:39:04 +0100

    Dodanie funkcji do menu

--- projekt1.cpp
+++ projekt1.cpp
@@ -29,8 +29,11 @@ int main() {
         cout << endl;
         cout << "MENU" << endl;
         cout << "Podaj numer czynnosci, ktora chcesz wykonac" << endl;
+	cout << "1. Silnia"<<endl;
 	cout << "0. Wyjscie" << endl;
         cin >> wyjscie;
+	if(wyjscie == 1)
+		silnia(a,b);
     } while(wyjscie != 0);
     return 0;
 }
