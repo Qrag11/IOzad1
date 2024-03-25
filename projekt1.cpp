a46f8f2c19fe608df1a8fc91c88413b7ca6de4ca
Qrag11 <m.przystawski8@gmail.com>
2024-03-25 17:37:58 +0100

    Dodanie funkcji silnia

--- projekt1.cpp
+++ projekt1.cpp
@@ -2,6 +2,23 @@
 
 using namespace std;
 
+void silnia(int a, int b)
+{
+    int wynikA = 1, wynikB = 1;
+
+    for(int i=1;i<=a;i++)
+    {
+       wynikA = wynikA*i;
+    }
+    for(int i=1;i<=b;i++)
+    {
+       wynikB = wynikB*i;
+    }
+
+    cout<<"Silnia z a = "<<wynikA<<endl;
+    cout<<"Silnia z b = "<<wynikB<<endl;
+}
+
 int main() {
     int a, b;
     cout << "Podaj liczbe calkowita ";
@@ -12,7 +29,7 @@ int main() {
         cout << endl;
         cout << "MENU" << endl;
         cout << "Podaj numer czynnosci, ktora chcesz wykonac" << endl;
-        cout << "0. Wyjscie" << endl;
+	cout << "0. Wyjscie" << endl;
         cin >> wyjscie;
     } while(wyjscie != 0);
     return 0;
